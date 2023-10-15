4a61d3339bf60edd7bae50908cff8fc79174790b
Sumit <itssumit258@gmail.com>
2023-10-15 15:53:03 +0530

    feat. bottom menu write icon border

--- components/BottomMenu.tsx
+++ components/BottomMenu.tsx
@@ -6,14 +6,14 @@ import { CgProfile } from "react-icons/cg";
 const BottomMenu = () => {
   return (
     <div className="w-screen bg-transparent flex justify-center items-center">
-      <div className=" w-[90%] fixed bottom-0  mb-4 border border-grey-300 pt-7 pb-7 pl-2 pr-2 bottomMenu-shadow rounded-lg flex justify-evenly items-center overflow-visible">
+      <div className="bottomMenu-shadow  w-[90%] fixed bottom-0  mb-4 border border-grey-300 pt-7 pb-7 pl-2 pr-2 rounded-lg flex justify-evenly items-center overflow-visible">
         <LuHome className="w-7 h-7" />
 
         <MdOutlineExplore className="w-7 h-7" />
 
-        <div className="relative w-12 h-12 overflow-visible flex justify-center items-center">
+        <div className="pen relative w-12 h-12 overflow-visible flex justify-center items-center">
           <FaPenNib
-            className=" pen w-7 h-7  absolute z-3 bottom-11"
+            className="  w-7 h-7  absolute z-3 bottom-11  text-white"
             style={{ transform: "rotate(135deg)" }}
           />
         </div>
