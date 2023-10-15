96468ba2b108e9c630843ce2389d336f8f22deda
Sumit <itssumit258@gmail.com>
2023-10-15 20:09:30 +0530

    fix: text size on whatsapp action button

--- components/WhatsappButton.tsx
+++ components/WhatsappButton.tsx
@@ -6,7 +6,7 @@ const WhatsappButton = () => {
       <a
         href="https://whatsapp.com/channel/0029Va8lsG74tRrzqkQjUC3C"
         target="_blank"
-        className="inline-flex p-4  text-white justify-center items-center gap-2"
+        className="inline-flex p-4  text-white justify-center items-center gap-2 text-xs md:text-sm lg:text-base"
       >
         <BsWhatsapp className="w-8 h-8" />
         Get Shayari on your Whatsapp
