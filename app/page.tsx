d58f63b3853fd68ed97050d315a234cf75be30df
Sumit <itssumit258@gmail.com>
2023-10-15 18:01:28 +0530

    fix: height of the page for mobile screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { WelcomeText, WelcomeShayariCard, SearchShayari } from "@/components";
 
 export default function Home() {
   return (
-    <main className="w-full md:w-[70%] lg:w-[45%]  p-5 flex flex-col justify-center items-center">
+    <main className="w-full h-[svh] md:w-[70%] lg:w-[45%]  p-5 flex flex-col justify-center items-center">
       <WelcomeText />
       <WelcomeShayariCard />
       <SearchShayari />
